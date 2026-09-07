542de13ecce568c94807a2f6f01d94885a01e2cd
Lucky <luckyraj2901@gmail.com>
2026-09-08 00:39:10 +0530

    feat: initialize project with Expo, theme tokens, and tab navigator

--- src/navigator/AppNavigator.jsx
+++ src/navigator/AppNavigator.jsx
@@ -1,36 +1,56 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CustomFloatingTabBar from '../components/navigations/CustomFloatingTabBar';
 
-import HomeScreen from "../screens/HomeScreen";
-import ShopScreen from "../screens/ShopScreen";
-import EMIDuesScreen from "../screens/EMIDues";
-import LimitScreen from "../screens/LimitScreen";
-import ProfileScreen from "../screens/ProfileScreen";
+import HomeScreen from '../screens/HomeScreen';
+import ShopScreen from '../screens/ShopScreen';
+import EMIDuesScreen from '../screens/EMIDues';
+import LimitScreen from '../screens/LimitScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
+import ProductDetailsScreen from '../screens/ProductDetailsScreen';
+import EMIScreen from '../screens/EMIScreen';
+import OrderSummaryScreen from '../screens/OrderSummaryScreen';
+import OrderSuccessScreen from '../screens/OrderSuccessScreen';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function MainTabNavigator() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Shop"
+      tabBar={(props) => <CustomFloatingTabBar {...props} />}
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Shop" component={ShopScreen} />
+      <Tab.Screen name="EMI Dues" component={EMIDuesScreen} />
+      <Tab.Screen name="Limit" component={LimitScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function AppNavigator() {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator
-                initialRouteName="Home"
-                tabBar={(props) => (
-                    <CustomFloatingTabBar {...props} />
-                )}
-                screenOptions={{
-                    headerShown: false,
-                }}
-            >
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Shop" component={ShopScreen} />
-                <Tab.Screen name="EMI Dues" component={EMIDuesScreen} />
-                <Tab.Screen name="Limit" component={LimitScreen} />
-                <Tab.Screen name="Profile" component={ProfileScreen} />
-            </Tab.Navigator>
-
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          animation: 'slide_from_right',
+        }}
+      >
+        <Stack.Screen name="MainTabs" component={MainTabNavigator} />
+        <Stack.Screen name="ProductDetails" component={ProductDetailsScreen} />
+        <Stack.Screen name="EMISelect" component={EMIScreen} />
+        <Stack.Screen name="OrderSummary" component={OrderSummaryScreen} />
+        <Stack.Screen name="OrderSuccess" component={OrderSuccessScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
